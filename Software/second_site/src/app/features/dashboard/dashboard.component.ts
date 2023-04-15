58251ee4f11bb650588478f31ff0fa706e0e4acf
Irakli Turabelidze <turabelidze.irakli@gmail.com>
2023-04-15 15:05:58 +0400

    import apiservice in dashboard

--- Software/second_site/src/app/features/dashboard/dashboard.component.ts
+++ Software/second_site/src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   isToggleChecked = true;
 
-  constructor() { }
+  constructor(_api: ApiService) { }
 
   ngOnInit(): void {
   }
