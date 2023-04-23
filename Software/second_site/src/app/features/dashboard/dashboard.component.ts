a7fc57bca0211cbba1c1bb037f8110494a7d616b
Irakli Turabelidze <turabelidze.irakli@gmail.com>
2023-04-23 10:21:51 +0400

    added all chart data

--- Software/second_site/src/app/features/dashboard/dashboard.component.ts
+++ Software/second_site/src/app/features/dashboard/dashboard.component.ts
@@ -63,6 +63,72 @@ export class DashboardComponent implements OnInit {
       });
       this.createLineCharts();
     });
+
+    this._api.RecentMeasurementPost(2, 1, 30).subscribe((res) => {
+      this.lineChartCon2 = res.map((item: any) => item.value);
+      this.lineChartLabels2 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart2.data.datasets[0].data = this.lineChartCon2;
+      this.lineChart2.data.labels = this.lineChartLabels2;
+      this.lineChart2.update();
+    });
+
+    this._api.RecentMeasurementPost(3, 1, 30).subscribe((res) => {
+      this.lineChartCon3 = res.map((item: any) => item.value);
+      this.lineChartLabels3 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart3.data.datasets[0].data = this.lineChartCon3;
+      this.lineChart3.data.labels = this.lineChartLabels3;
+      this.lineChart3.update();
+    });
+
+    this._api.RecentMeasurementPost(6, 1, 30).subscribe((res) => {
+      this.lineChartCon4 = res.map((item: any) => item.value);
+      this.lineChartLabels4 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart4.data.datasets[0].data = this.lineChartCon4;
+      this.lineChart4.data.labels = this.lineChartLabels4;
+      this.lineChart4.update();
+    });
+
+    this._api.RecentMeasurementPost(5, 1, 30).subscribe((res) => {
+      this.lineChartCon5 = res.map((item: any) => item.value);
+      this.lineChartLabels5 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart5.data.datasets[0].data = this.lineChartCon5;
+      this.lineChart5.data.labels = this.lineChartLabels5;
+      this.lineChart5.update();
+    });
+
+    this._api.RecentMeasurementPost(4, 1, 30).subscribe((res) => {
+      this.lineChartCon6 = res.map((item: any) => item.value);
+      this.lineChartLabels6 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart6.data.datasets[0].data = this.lineChartCon6;
+      this.lineChart6.data.labels = this.lineChartLabels6;
+      this.lineChart6.update();
+    });
+
+    setInterval(() => {
+      const params = this.serviceParams[this.selectedOption];
+      this.send(this.serviceParams[this.selectedOption].timeType, this.serviceParams[this.selectedOption].time);
+    }, 3000);
+
   }
 
   clicked() {
@@ -117,7 +183,7 @@ export class DashboardComponent implements OnInit {
             data: chartData,
             backgroundColor: "limegreen",
             borderColor: "#7ADC24",
-            tension: 0.2,
+            tension: 0.4,
           },
         ],
       },
@@ -142,6 +208,66 @@ export class DashboardComponent implements OnInit {
       this.lineChart1.data.datasets[0].data = this.lineChartCon1;
       this.lineChart1.data.labels = this.lineChartLabels1;
       this.lineChart1.update();
+    });
+
+    this._api.RecentMeasurementPost(2, timeType, time).subscribe((res) => {
+      this.lineChartCon2 = res.map((item: any) => item.value);
+      this.lineChartLabels2 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart2.data.datasets[0].data = this.lineChartCon2;
+      this.lineChart2.data.labels = this.lineChartLabels2;
+      this.lineChart2.update();
+    });
+
+    this._api.RecentMeasurementPost(3, timeType, time).subscribe((res) => {
+      this.lineChartCon3 = res.map((item: any) => item.value);
+      this.lineChartLabels3 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart3.data.datasets[0].data = this.lineChartCon3;
+      this.lineChart3.data.labels = this.lineChartLabels3;
+      this.lineChart3.update();
+    });
+
+    this._api.RecentMeasurementPost(6, timeType, time).subscribe((res) => {
+      this.lineChartCon4 = res.map((item: any) => item.value);
+      this.lineChartLabels4 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart4.data.datasets[0].data = this.lineChartCon4;
+      this.lineChart4.data.labels = this.lineChartLabels4;
+      this.lineChart4.update();
+    });
+
+    this._api.RecentMeasurementPost(5, timeType, time).subscribe((res) => {
+      this.lineChartCon5 = res.map((item: any) => item.value);
+      this.lineChartLabels5 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart5.data.datasets[0].data = this.lineChartCon5;
+      this.lineChart5.data.labels = this.lineChartLabels5;
+      this.lineChart5.update();
+    });
+
+    this._api.RecentMeasurementPost(4, timeType, time).subscribe((res) => {
+      this.lineChartCon6 = res.map((item: any) => item.value);
+      this.lineChartLabels6 = res.map((item: any) => {
+        const date = new Date(item.time);
+        date.setHours(date.getHours() + 4);
+        return date.toISOString().slice(11, 19);
+      });
+      this.lineChart6.data.datasets[0].data = this.lineChartCon6;
+      this.lineChart6.data.labels = this.lineChartLabels6;
+      this.lineChart6.update();
     });
   }
 }
